07925b65618971e7b21b810e14d691365f182ed0
Jin <392637221@qq.com>
2026-06-29 08:27:09 +0800

    fix: replace comment controller to fix cmt is not defined error

--- artitalk01/patch.js
+++ artitalk01/patch.js
@@ -40,4 +40,83 @@ if (siteName && fs.existsSync(controllerPath)) {
   }
 }
 
+// Patch 4: Replace comment controller to fix "cmt is not defined" error
+const commentCtrlPath = path.join(__dirname, 'node_modules/@waline/vercel/src/controller/comment.js');
+if (fs.existsSync(commentCtrlPath)) {
+  const simplifiedComment = `const BaseRest = require('./rest.js');
+
+module.exports = class extends BaseRest {
+  constructor(ctx) {
+    super(ctx);
+    this.modelInstance = this.getModel('Comment');
+  }
+
+  async getAction() {
+    const { path: url, page, pageSize, sortBy } = this.get();
+    const where = { url };
+    const count = await this.modelInstance.count(where);
+    const totalPages = Math.ceil(count / pageSize);
+    const spamCount = await this.modelInstance.count({ ...where, status: 'spam' });
+    const waitingCount = await this.modelInstance.count({ ...where, status: 'waiting' });
+    const comments = await this.modelInstance.select(where, {
+      desc: sortBy === 'time' ? 'insertedAt' : 'like',
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
+    });
+    return this.json({
+      errno: 0, errmsg: '',
+      data: {
+        page, totalPages, pageSize,
+        count: count - spamCount - waitingCount,
+        data: comments.map(c => {
+          c.time = new Date(c.insertedAt).getTime();
+          delete c.insertedAt; delete c.createdAt; delete c.updatedAt;
+          c.like = Number(c.like) || 0;
+          c.children = [];
+          return c;
+        }),
+      },
+    });
+  }
+
+  async postAction() {
+    const { comment, link, mail, nick, pid, rid, ua, url } = this.post();
+    const data = {
+      link, mail, nick, pid, rid, ua, url, comment,
+      ip: this.ctx.ip,
+      insertedAt: new Date(),
+      user_id: '',
+      status: 'approved',
+    };
+    const { userInfo } = this.ctx.state;
+    if (userInfo && userInfo.objectId) data.user_id = userInfo.objectId;
+    try {
+      const resp = await this.modelInstance.add(data);
+      return this.json({ errno: 0, errmsg: '', data: resp });
+    } catch (err) {
+      console.error('[SIMPLE_POST] Error:', err.message);
+      return this.json({ errno: 1, errmsg: err.message });
+    }
+  }
+
+  async deleteAction() {
+    const { userInfo } = this.ctx.state;
+    if (!userInfo || userInfo.type !== 'administrator') return this.fail(403);
+    await this.modelInstance.delete({ objectId: this.id });
+    return this.success();
+  }
+
+  async putAction() {
+    const { userInfo } = this.ctx.state;
+    if (!userInfo || userInfo.type !== 'administrator') return this.fail(403);
+    const data = this.post();
+    await this.modelInstance.update(data, { objectId: this.id });
+    return this.success();
+  }
+};
+`;
+  fs.writeFileSync(commentCtrlPath, simplifiedComment);
+  console.log('[patch] comment.js replaced with simplified version (fix cmt error)');
+}
+
 console.log('[patch] Done');
